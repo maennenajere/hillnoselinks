4e4598abc4a710d53402b4013d9be1ccb7c3d2fd
Jere Mäennenä <jere.maennena@gmail.com>
2025-11-08 21:25:16 +0000

    Add Quote component and integrate Supabase for fetching quotes; update Footer and App structure

---
+++ src/components/Quote.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabase'
+
+export default function Quotes() {
+    const [quote, setQuote] = useState(null)
+
+    useEffect(() => {
+        async function fetchQuote() {
+            const { data, error } = await supabase
+                .from('quotes')
+                .select('quote, date')
+                .order('date', { ascending: false })
+                .limit(1)
+                .single()
+
+            if (error) {
+                console.error('Error fetching:', error)
+                setQuote({
+                    greeting: 'Hello traveler!',
+                    date: new Date().toISOString(),
+                    _fallback: true,
+                })
+            } else {
+                setQuote(data)
+            }
+        }
+
+        fetchQuote()
+    }, [])
+
+    if (!quote) {
+        console.log("table error")
+        return null
+    }
+
+    return (
+        <p className="text-gray-400 italic text-sm">
+            “{quote.quote}” <br />
+            <span className="text-gray-600 text-xs">
+        {new Date(quote.date).toLocaleDateString('fi-FI', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+        })}
+      </span>
+        </p>
+    )
+}
